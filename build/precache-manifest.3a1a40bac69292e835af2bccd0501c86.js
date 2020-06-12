42621b4ced9697b016333d6bbc6d252fd90730f6
Jim Redfern <jim@tradeshift.com>
2020-06-12 10:58:32 -0700

    Add client build dir

---
+++ build/precache-manifest.3a1a40bac69292e835af2bccd0501c86.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591ee59778dd192934156263b401741d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "240b828fdeff0ee51570",
+    "url": "/static/css/main.e5d93194.chunk.css"
+  },
+  {
+    "revision": "dabeba64656492cab1e6",
+    "url": "/static/js/2.a74ed8da.chunk.js"
+  },
+  {
+    "revision": "ccc53a9fa0a50acd3ac83789c3194fa4",
+    "url": "/static/js/2.a74ed8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240b828fdeff0ee51570",
+    "url": "/static/js/main.eae8a411.chunk.js"
+  },
+  {
+    "revision": "728ab315c10d85f5443f",
+    "url": "/static/js/runtime-main.40b819e4.js"
+  },
+  {
+    "revision": "44ea8275e878ed72a9c8a393f9de12d2",
+    "url": "/static/media/Muli-Bold.44ea8275.ttf"
+  },
+  {
+    "revision": "42ba31045765427e247a38c7e2c50e41",
+    "url": "/static/media/Muli-Italic.42ba3104.ttf"
+  },
+  {
+    "revision": "e063a333cc9d58a3ad85d5cd86bf92d5",
+    "url": "/static/media/Muli-Regular.e063a333.ttf"
+  },
+  {
+    "revision": "683fbda80ae8ede5444b9b86406aeae3",
+    "url": "/static/media/Tradeshift-Display-Medium.683fbda8.woff2"
+  },
+  {
+    "revision": "2a95e580eb44bf551048530b7d8a4860",
+    "url": "/static/media/bird-2.2a95e580.svg"
+  },
+  {
+    "revision": "87ace9d58b99c9e17d124931c33b5aa1",
+    "url": "/static/media/bird.87ace9d5.svg"
+  },
+  {
+    "revision": "c04ddbb948913e3926318aa306c9c24b",
+    "url": "/static/media/chicken.c04ddbb9.svg"
+  },
+  {
+    "revision": "6a6cc07486408ffbdefd815026a72afc",
+    "url": "/static/media/deer-black.6a6cc074.svg"
+  },
+  {
+    "revision": "c66a825a6065cc4b3f478ddf372d68c5",
+    "url": "/static/media/fox.c66a825a.svg"
+  },
+  {
+    "revision": "0b7b275b7ff54479427cd8e439fee478",
+    "url": "/static/media/hedgehog.0b7b275b.svg"
+  },
+  {
+    "revision": "fd0a14cc68eef63e5db668861558a9e1",
+    "url": "/static/media/rabbit.fd0a14cc.svg"
+  },
+  {
+    "revision": "57ea8deb415a7e4db0ada75d09aed2d6",
+    "url": "/static/media/squirrel.57ea8deb.svg"
+  }
+]);
