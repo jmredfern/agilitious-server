a1d5748b616669af65e6496d69f440dc9b3db229
Jim Redfern <jim@tradeshift.com>
2020-07-25 13:30:38 -0700

    Tidy up events some more
    Only send values used by the client, apart from ones that may be useful
    for features.

--- src/util/websocket.ts
+++ src/util/websocket.ts
@@ -1,7 +1,7 @@
 'use strict';
 import { getLoggerByFilename, trimString } from '../util/logger';
 import WebSocket from 'ws';
-import { Player, Event, ClientEvent } from '../types';
+import { Player, Event, ClientEvent, UUID } from '../types';
 import { Logger } from 'log4js';
 import { inspect } from 'util';
 
@@ -19,9 +19,11 @@ const sendEvent = (websocket: WebSocket, event: Event): void => {
 	}
 };
 
-export const sendServerEvent = <E extends Event>(player: Player, event: E): void => {
+export const sendServerEvent = <E extends Event>(player: Player, gameId: UUID, event: E): void => {
 	const { websocket, playerId } = player;
-	log.info(`Sending server event ${event.type} to playerId ${playerId} ${trimString(inspect(event))}`);
+	log.info(
+		`Sending server event ${event.type} to gameId ${gameId}, playerId ${playerId} ${trimString(inspect(event))}`,
+	);
 	sendEvent(websocket, event);
 };
 
